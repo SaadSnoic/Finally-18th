78fe1e6044f0689a4a81abedcc0828f78aaae817
SaadSnoic <88655839+SaadSnoic@users.noreply.github.com>
2024-04-19 20:02:23 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2024',
-  name: 'Wifey'
+  birthdate: 'April 21, 2024',
+  name: 'Julebi'
 };
 
 function hideEverything() {
